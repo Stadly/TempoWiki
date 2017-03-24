ab31a1aecdb3eed9852ae5bcd3bd58c965096d5d
Stadly <magnar@myrtveit.com>
2017-03-24 20:33:06 +0100

    Increase security
    FIX: Update .htaccess and initiation script

--- server/includes/initiate.php
+++ server/includes/initiate.php
@@ -3,10 +3,9 @@
 session_start();
 session_cache_limiter('nocache');
 
-define('DEVELOP_MODE', strpos($_SERVER['HTTP_HOST'], 'tempowiki.com') !== FALSE);
-define('DATABASE_HOST', DEVELOP_MODE ? 'localhost' : 'myrtveitfoto.no.mysql');
-define('DATABASE_NAME', DEVELOP_MODE ? 'sp_tempowiki' : 'myrtveitfoto_no');
-define('DATABASE_USERNAME', DEVELOP_MODE ? 'root' : 'myrtveitfoto_no');
-define('DATABASE_PASSWORD', DEVELOP_MODE ? '' : 'oJTF2MB1');
+define('DATABASE_HOST', 'hostname');
+define('DATABASE_NAME', 'database');
+define('DATABASE_USERNAME', 'username');
+define('DATABASE_PASSWORD', 'password');
 
 ?>
